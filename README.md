31af31486a1aec4b284a7f9c03372bd2daf5049e
yanrujhou <joe04024@gmail.com>
2019-11-15 20:36:05 -0800

    add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+
+To install:
+
+    pip install -r requirements.txt 
+
+To run program:
+    
+     ./downloader <URL> -c nThreads
+ 
+Example:
+    ./Downloader.py http://oak.cs.ucla.edu/classes/cs246/projects/p3_test.sh
+    ./Downloader.py http://oak.cs.ucla.edu/classes/cs246/projects/p3_test.sh -c 5
--- TestSuiteFolder/test_1.py
+++ TestSuiteFolder/test_1.py
@@ -6,16 +6,21 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
 
+    #sample file test
     def test_1(self):
         url = 'http://oak.cs.ucla.edu/classes/cs246/projects/p3_test.sh'
         runner = CliRunner()
         result = runner.invoke(
             download_file, [url,'-c','5'])
         self.assertEqual(0, result.exit_code)
-        # self.assertIn('Find: 3 sample', result.output)
 
-
-        # download_file(obj={})
+    #mp3 file test
+    def test_2(self):
+        url = "http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"
+        runner = CliRunner()
+        result = runner.invoke(
+            download_file, [url, '-c', '5'])
+        self.assertEqual(0, result.exit_code)
 
 if __name__ == '__main__':
     unittest.main()
